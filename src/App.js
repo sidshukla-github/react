d636c59191c52da3163748041a3b4eb803aedb7d
Siddarth Shukla <sidshukla@deloitte.com.au>
2020-03-27 13:58:35 +1100

    first commit

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+/*
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          This is react app
+        </p>
+
+      </header>
+    </div>
+  );
+}
+*/
+class App extends React.Component
+{
+  render(){
+  return (
+    <div>
+      <div><h1> hellow mate</h1></div>
+   <script src ='https://deloitte01.pegalabs.io/prweb/PRServlet/app/SR_9831/?pzuiactionzzz=CXtpbn0vY0o1cm11VjY5VnNxUGpDcm1TVm4rSDJSZHVsVm9nMXB1dkxsYlB0NVVMczFZTlEwVyt1%0AVFBRcjg4bDFJN2VX'></script>
+<div data-pega-encrypted ='true' 
+data-pega-encrypted-hash = 'pzuiactionzzz=CXtpbn14bzVETU5jQk1NSGJaclRxR09Wb2thbmxxeVpqb21QSTIwbm10OEpqU2x2a1VteXZXVmpw%0AeXk4OVIyTjZBc082NjR4Wi83U0E3NkF0cmh6T0pWUVRCTEZEZjEvR05jZ0NBdzNqS2JVN2o0dU4y%0AaitLZlZabDRDVG13NVRJN2U0bHY3NHJMV2M4NWN5V3RDbmVNc0lQTm5rVStzeXhlYWJPKzB0TWhH%0Ac2FROFE4WjN0NTY0N3Vyc2poOXd4T2Q1RFh3dVRLS2JEWTRLRGFOSUhpY2pQYUVBPT0%3D' 
+data-pega-gadgetname ='PegaGadget' 
+data-pega-action ='createNewWork' 
+data-pega-action-param-classname ='Del-SR-Work-Servicing' 
+data-pega-action-param-flowname ='pyStartCase' 
+data-pega-isdeferloaded ='false' 
+data-pega-applicationname ='SR' 
+data-pega-threadname ='STANDARD' 
+data-pega-resizetype ='stretch' 
+data-pega-url ='https://deloitte01.pegalabs.io/prweb/PRServlet/app/SR_9831/' ></div>
+    </div>
+    
+   ) }
+}
+
+export default App;
